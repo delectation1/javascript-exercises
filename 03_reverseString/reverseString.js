4008dc7db1ef51b020796569b5f9b34e6cebabf1
delectation <wongyhpersonal@gmail.com>
2023-05-26 21:34:35 +0800

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
+    let oldString = string;
 
+    for (let i = oldString.length - 1; i > -1; i--) {
+        newString += oldString[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
