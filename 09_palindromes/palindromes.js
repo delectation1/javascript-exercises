90acf69d679612a24a2e98fe4afbfd166d237def
delectation <wongyhpersonal@gmail.com>
2023-05-28 18:17:49 +0800

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()/\s]/g,"");
+    let reversedString = newString.split("").reverse().join("");
+    if (reversedString == newString) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
