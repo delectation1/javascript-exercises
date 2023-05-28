62e7429779020ebaaf234f5c9ecc3f7f882dee2d
delectation <wongyhpersonal@gmail.com>
2023-05-28 14:18:09 +0800

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    // validate user response
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    // initialize variable to hold final sum, start and end number of loop
+    let sum = 0;
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
