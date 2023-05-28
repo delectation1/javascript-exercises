6b28ad695a83afccc0787031f37d8e98270736c5
delectation <wongyhpersonal@gmail.com>
2023-05-28 18:30:10 +0800

    Finish 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    /* index into subarrays to get title 
+    add it to our newArray */
+    const booksArray = [];
+    for (let i = 0; i < array.length; i++) {
+        booksArray.push(array[i].title);
+    }
+    return booksArray;
 };
 
 // Do not edit below this line
