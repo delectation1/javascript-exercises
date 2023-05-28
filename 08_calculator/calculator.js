8f8e7a6c80dce0e17fa263d1bed5118a8d873493
delectation <wongyhpersonal@gmail.com>
2023-05-28 15:25:55 +0800

    FInish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(...args) {
+	let added = 0;
+  args.forEach( (number) => added += number);
+  return added;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach( (number) => sum += number);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = array[0];
+  for (let i = 1; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  // base case
+	if (num == 0) {
+    return 1;
+  }
+
+  return (num * factorial(num-1));
 };
 
 // Do not edit below this line
