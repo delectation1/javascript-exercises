622957ef8a041b3572d148f0859ba77d09cbbcdf
delectation <wongyhpersonal@gmail.com>
2023-05-28 17:28:15 +0800

    Finish 09-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    // store fibonacci numbers into array to index to get member
+    const sequence = [1,1];
+    if (num < 0) {
+        return "OOPS"
+    }
+    let number = parseInt(num);
+
+    for (let i = 0; i < number - 1; i++) {
+        let nextFib = sequence[i] + sequence[i+1];
+        sequence.push(nextFib);
+    }
+
+    return sequence[number - 1];
 };
 
 // Do not edit below this line
